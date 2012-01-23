ac7e25347c0aec07dacf6d51326c8b6c36c4bd53
Sergio Campamá <sergiocampama@gmail.com>
2012-01-23 20:57:43 -0300

    fixes object error

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -3,7 +3,7 @@ var socket = io.connect('http://192.168.10.123:8080');
 $("#enter_button").click(function(){
 	console.log($("#current_text").val());
 	socket.emit('chat',$("#current_text").val());
-	$("#previous_chat_section").append($("#current_text").val('')+'<br/>');
+	$("#previous_chat_section").append($("#current_text").val()+'<br/>');
 	$("#current_text").val('');
 });
 
