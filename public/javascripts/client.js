67b8523732fa1ff4d27dd7b80cfd7780e558f2cf
Sergio Campamá <sergiocampama@gmail.com>
2012-01-23 20:53:54 -0300

    chat funcional

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,13 @@
 var socket = io.connect('http://192.168.10.123:8080');
 
+$("#enter_button").click(function(){
+	console.log($("#current_text").val());
+	socket.emit('chat',$("#current_text").val());
+	$("#previous_chat_section").append($("#current_text").val('')+'<br/>');
+	$("#current_text").val('');
+});
+
 socket.on('chat', function (data) {
 	console.log(data);
-	$("#previous_chat_section").append(data);
+	$("#previous_chat_section").append(data.message+'<br/>');
 });
