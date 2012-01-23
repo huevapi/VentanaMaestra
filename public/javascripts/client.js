061feb050df741443ed2292b7774710d20eb3327
Sergio Campamá <sergiocampama@gmail.com>
2012-01-23 20:37:39 -0300

    arreglando la ip del servidor del socket

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,6 +1,6 @@
-var socket = io.connect('http://192.168.10.23:8080');
+var socket = io.connect('http://192.168.10.123:8080');
 
 socket.on('chat', function (data) {
 	console.log(data);
-	
+	$("#previous_chat_section").append(data);
 });
