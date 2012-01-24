24ae0de751542d4e158b473a1b65b3077059ef0c
Sergio Campamá <sergiocampama@gmail.com>
2012-01-23 21:14:00 -0300

    agregando scroll to bottom

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -7,6 +7,7 @@ $("#current_text").keypress(function(e){
 	if (code == 13){
 		socket.emit('chat',$("#current_text").val());
 		$("#previous_chat_section").append(String($("#current_text").val())+'<br/>');
+		$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
 		$("#current_text").val('');
 	}
 });
@@ -15,6 +16,7 @@ $("#enter_button").click(function(){
 	console.log($("#current_text").val());
 	socket.emit('chat',$("#current_text").val());
 	$("#previous_chat_section").append(String($("#current_text").val())+'<br/>');
+	$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
 	$("#current_text").val('');
 });
 
@@ -24,8 +26,10 @@ socket.on('chat', function (data) {
 	if (data instanceof Array) {
 		$.each(data, function(index, value) { 
 		  $("#previous_chat_section").append(String(value.message)+'<br/>');
+		$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
 		});
 	} else {
 		$("#previous_chat_section").append(String(data.message)+'<br/>');
+		$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
 	}
 });
