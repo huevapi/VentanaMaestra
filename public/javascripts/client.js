98aa8b2c907d0994f2d7bfb1d8b23f848d5956c3
Sergio Campamá <sergiocampama@gmail.com>
2012-01-23 21:21:13 -0300

    sacando el scroll

--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -6,8 +6,7 @@ $("#current_text").keypress(function(e){
 	code= (e.keyCode ? e.keyCode : e.which);
 	if (code == 13){
 		socket.emit('chat',$("#current_text").val());
-		$("#previous_chat_section").append(String($("#current_text").val())+'<br/>');
-		$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
+		$("#previous_chat_section").prepend(String($("#current_text").val())+'<br/>');
 		$("#current_text").val('');
 	}
 });
@@ -15,8 +14,7 @@ $("#current_text").keypress(function(e){
 $("#enter_button").click(function(){
 	console.log($("#current_text").val());
 	socket.emit('chat',$("#current_text").val());
-	$("#previous_chat_section").append(String($("#current_text").val())+'<br/>');
-	$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
+	$("#previous_chat_section").prepend(String($("#current_text").val())+'<br/>');
 	$("#current_text").val('');
 });
 
@@ -25,11 +23,9 @@ socket.on('chat', function (data) {
 	
 	if (data instanceof Array) {
 		$.each(data, function(index, value) { 
-		  $("#previous_chat_section").append(String(value.message)+'<br/>');
-		$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
+		  $("#previous_chat_section").prepend(String(value.message)+'<br/>');
 		});
 	} else {
-		$("#previous_chat_section").append(String(data.message)+'<br/>');
-		$("#previous_chat_section").animate({ scrollTop: $(document).height() }, 500);
+		$("#previous_chat_section").prepend(String(data.message)+'<br/>');
 	}
 });
